3cc15cae4166d7f86d667fafb43b3f444e089377
TrivikramSiddavatam <46970554+TrivikramSiddavatam@users.noreply.github.com>
2021-08-13 22:13:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-print("Hello, World..!")
+# print("Hello, World..!")
 
 #Day#01/100 Exercise - Print
-print("")
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# print("")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
 
-#String Manipulation
-print("")
-print("Hello, World..! \nThis is Trivikram Here")
+# #String Manipulation
+# print("")
+# print("Hello, World..! \nThis is Trivikram Here")
 
-print("")
-print("Hello"+"Trivikram")
-print("Hello"+" "+"Trivikram")
+# print("")
+# print("Hello"+"Trivikram")
+# print("Hello"+" "+"Trivikram")
 
 
-print("")
+# print("")
 #Exercise 1-2 : Fix the code below 👇
 '''
 print(Day 1 - String Manipulation")
@@ -24,7 +24,42 @@ print("String Concatenation is done with the "+" sign.")
 print(("New lines can be created with a backslash and n.")
 '''
 #Solution
-print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+#The Python Input Function
+# print("Hellooo "+input("what is your Name: ")+"!")
+
+# Write a program that prints the number of characters in a user's name.
+# Solution
+# print(len(input("What is your Name? ")))
+
+# Variables
+# name = input("What is your Name? ")
+# length = len(name)
+# print(length)
+
+# Instructions
+# Write a program that switches the values stored in the variables a and b. 
+
+# a = input("a: ")
+# b = input("b: ")
+# c = a
+# a = b
+# b = c
+# print("a: " + a)
+# print("b: " + b)
+
+#Summary of all the above combined
+# Program: Band Name Generator Online
+#1. Create a greeting for your program.
+print("Welcome to the Band name Generator Online")
+#2. Ask the user for the city that they grew up in.
+City = input("Please Enter your City Name:\n")
+#3. Ask the user for the name of a pet.
+Pet_Name = input("Please enter your Pet Name:\n")
+#4. Combine the name of their city and pet and show them their band name.
+#5. Make sure the input cursor shows on a new line
+print("Your band name could be "+City+" "+Pet_Name+ "\n")
