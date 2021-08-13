de093e409873abe21e0d11434ef51ee237cc7617
Trivikram <46970554+TrivikramSiddavatam@users.noreply.github.com>
2021-08-13 13:53:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+print("Hello, World..!")
+
+#Day#01/100 Exercise - Print
+print("")
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#String Manipulation
+print("")
+print("Hello, World..! \nThis is Trivikram Here")
+
+print("")
+print("Hello"+"Trivikram")
+print("Hello"+" "+"Trivikram")
+
+
+print("")
+#Exercise 1-2 : Fix the code below 👇
+'''
+print(Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
+'''
+#Solution
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
